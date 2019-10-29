014e5c2ffd784e68c9d5631e094a7c931314ed4c
Anthony Pardo <pardo.anthony@gmail.com>
2019-10-29 21:23:14 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -12,7 +12,7 @@ def print_first_directors_movie_titles
   hoh = directors_database
   row_index = 0 
   while row_index < hoh.length do 
-    puts "#{hoh[0][:movies][0]}\n"
+    puts "#{hoh[0][:movies][0][:title]}\n"
     row_index += 1 
   end
 end
